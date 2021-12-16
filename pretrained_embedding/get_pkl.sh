c39c672e0b53b035c0024b6d46372ef406ba0671
justinyeh1995 <justinyeh1995@gmail.com>
2021-12-16 13:19:41 +0800

    a

--- pretrained_embedding/get_pkl.sh
+++ pretrained_embedding/get_pkl.sh
@@ -5,6 +5,5 @@ gunzip -c GoogleNews-vectors-negative300.bin.gz > model.bin
 rm GoogleNews-vectors-negative300.bin.gz 
 
 wget http://wikipedia2vec.s3.amazonaws.com/models/ja/2018-04-20/jawiki_20180420_300d.pkl.bz2
-bunzip2 wget http://wikipedia2vec.s3.amazonaws.com/models/ja/2018-04-20/jawiki_20180420_300d.pkl.bz2
-rm jawiki_20180420_300d.pkl.bz2
+bunzip2 http://wikipedia2vec.s3.amazonaws.com/models/ja/2018-04-20/jawiki_20180420_300d.pkl.bz2
 
